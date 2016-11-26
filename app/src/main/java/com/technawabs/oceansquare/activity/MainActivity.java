3fc56b148f7d38f14afd05cc5f29175e00248a53
Piyush Aggarwal <piyush.aggarwal@ibtechnology.com>
2016-11-26 18:56:42 +0530

    access_token

--- app/src/main/java/com/technawabs/oceansquare/activity/MainActivity.java
+++ app/src/main/java/com/technawabs/oceansquare/activity/MainActivity.java
@@ -2,6 +2,7 @@ package com.technawabs.oceansquare.activity;
 
 import android.app.ProgressDialog;
 import android.content.Context;
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
@@ -39,29 +40,6 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     String loginUrl;
     WebView browser;
 
-    final Context myApp = this;
-
-    @JavascriptInterface
-    public void processHTML(String html) throws JSONException {
-        if (html == null)
-            return;
-        else {
-
-            Document doc = Jsoup.parse(html);
-            Element link = doc.select("pre").first();
-            String finaltext = link.text();
-            JSONObject jsonObject = new JSONObject(finaltext);
-            Toast.makeText(MainActivity.this, jsonObject.getString("access_token"), Toast.LENGTH_SHORT).show();
-
-
-
-
-
-        }
-
-    }
-
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -70,28 +48,6 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         // Set a toolbar to replace the action bar.
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
-
-        browser = (WebView) findViewById(R.id.webview);
-        browser.setWebViewClient(new MyBrowser());
-
-        loginUrl = "https://cloud.digitalocean.com/v1/oauth/authorize?client_id=bc9ddc5faa820489de13ac23f998fe79d55a9e880b5faaa0353321630539822d&redirect_uri=http://104.131.39.100:8083/callback&response_type=code";
-        browser.getSettings().setLoadsImagesAutomatically(true);
-        browser.getSettings().setJavaScriptEnabled(true);
-        browser.addJavascriptInterface(this, "HTMLOUT");
-        browser.setScrollBarStyle(View.SCROLLBARS_INSIDE_OVERLAY);
-
-        browser.setWebViewClient(new WebViewClient() {
-            @Override
-            public void onPageFinished(WebView view, String url) {
-                browser.loadUrl("javascript:window.HTMLOUT.processHTML('<html>'+document.getElementsByTagName('html')[0].innerHTML+'</html>');");
-
-            }
-
-        });
-
-
-        browser.loadUrl(loginUrl);
-
 
         loginBtn = (Button) findViewById(R.id.loginBtn);
         loginBtn.setOnClickListener(this);
@@ -109,15 +65,7 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     }
 
     public void sendRequest() {
-
-
+        startActivity(new Intent(MainActivity.this, BrowserActivity.class));
     }
 
-    private class MyBrowser extends WebViewClient {
-        @Override
-        public boolean shouldOverrideUrlLoading(WebView view, String url) {
-            view.loadUrl(url);
-            return true;
-        }
-    }
 }
