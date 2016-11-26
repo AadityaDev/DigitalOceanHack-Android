d63680578f2383fbbbaab458bde71607a21d74d6
Piyush Aggarwal <piyush.aggarwal@ibtechnology.com>
2016-11-26 15:07:57 +0530

    adding login activity

--- app/src/main/java/com/technawabs/oceansquare/activity/MainActivity.java
+++ app/src/main/java/com/technawabs/oceansquare/activity/MainActivity.java
@@ -1,15 +1,85 @@
 package com.technawabs.oceansquare.activity;
 
+import android.app.ProgressDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
 
+import com.android.volley.Request;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.VolleyLog;
+import com.android.volley.toolbox.JsonObjectRequest;
 import com.technawabs.oceansquare.R;
+import com.technawabs.oceansquare.request.SingeltonRequest;
 
-public class MainActivity extends AppCompatActivity {
+import org.json.JSONObject;
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+
+    Button loginBtn;
+    String baseUrl;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        // Set a toolbar to replace the action bar.
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        loginBtn = (Button) findViewById(R.id.loginBtn);
+        loginBtn.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View view) {
+        int currentId = view.getId();
+
+        switch (currentId) {
+            case R.id.loginBtn:
+                sendRequest();
+                break;
+        }
+    }
+
+    public void sendRequest() {
+
+        String client_id = getResources().getString(R.string.client_id);
+        baseUrl = "https://cloud.digitalocean.com/v1/oauth/authorize?client_id=" + client_id;
+
+        String tag_json_obj = "json_obj_req";
+
+        final ProgressDialog pDialog = new ProgressDialog(this);
+        pDialog.setMessage("Loading...");
+        pDialog.show();
+
+        JsonObjectRequest jsonObjReq = new JsonObjectRequest(Request.Method.GET,
+                baseUrl, null,
+                new Response.Listener<JSONObject>() {
+
+                    @Override
+                    public void onResponse(JSONObject response) {
+                        Log.d("request", response.toString());
+                        pDialog.hide();
+                    }
+                }, new Response.ErrorListener() {
+
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                VolleyLog.d("request", "Error: " + error.getMessage());
+                // hide the progress dialog
+                pDialog.hide();
+            }
+        });
+
+        // Adding request to request queue
+        SingeltonRequest.getInstance().addToRequestQueue(jsonObjReq, tag_json_obj);
+
+
     }
 }
---
+++ app/src/main/java/com/technawabs/oceansquare/request/SingeltonRequest.java
@@ -0,0 +1,59 @@
+package com.technawabs.oceansquare.request;
+
+import android.app.Application;
+import android.text.TextUtils;
+
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.toolbox.ImageLoader;
+import com.android.volley.toolbox.Volley;
+
+/**
+ * Created by piyush.aggarwal on 26-11-2016.
+ */
+
+public class SingeltonRequest extends Application {
+
+    public static final String TAG = SingeltonRequest.class
+            .getSimpleName();
+
+    private RequestQueue mRequestQueue;
+    private ImageLoader mImageLoader;
+
+    private static SingeltonRequest mInstance;
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        mInstance = this;
+    }
+
+    public static synchronized SingeltonRequest getInstance() {
+        return mInstance;
+    }
+
+    public RequestQueue getRequestQueue() {
+        if (mRequestQueue == null) {
+            mRequestQueue = Volley.newRequestQueue(getApplicationContext());
+        }
+
+        return mRequestQueue;
+    }
+
+    public <T> void addToRequestQueue(Request<T> req, String tag) {
+        // set the default tag if tag is empty
+        req.setTag(TextUtils.isEmpty(tag) ? TAG : tag);
+        getRequestQueue().add(req);
+    }
+
+    public <T> void addToRequestQueue(Request<T> req) {
+        req.setTag(TAG);
+        getRequestQueue().add(req);
+    }
+
+    public void cancelPendingRequests(Object tag) {
+        if (mRequestQueue != null) {
+            mRequestQueue.cancelAll(tag);
+        }
+    }
+}
